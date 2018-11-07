dd859af9205c46209edb8a933ae455ca9d3503db
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-07 21:23:56 +0000

    Updates for TimeStampedModel

--- badger/models.py
+++ badger/models.py
@@ -1,16 +1,17 @@
+from model_utils.models import TimeStampedModel
 from django.db import models
 
-class Employee(models.TimeStampedModel):
+class Badge(TimeStampedModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Employee(TimeStampedModel):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     badge = models.ManyToManyField(Badge)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
-
-class Badge(models.TimeStampedModel):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
 
