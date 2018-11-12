831bcb85e50d09622e44752b7fdca29f18e653d7
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-12 15:14:47 +0000

    Added update and list routes.

--- badger/urls.py
+++ badger/urls.py
@@ -6,4 +6,6 @@ app_name = 'badger'
 
 urlpatterns = [
     path('add_employee/', views.EmployeeCreate.as_view(), name='add_employee'),
+    path('update_employee/<int:pk>/', views.EmployeeUpdate.as_view(), name='update_employee'),
+    path('list_employees/', views.EmployeeList.as_view(), name='list_employee'),
 ]
