050652843c6e3d55ed8d97c2cedfb18a9a3fe661
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-12 15:18:06 +0000

    Initial changes for Employee update and list

--- badger/views.py
+++ badger/views.py
@@ -1,8 +1,18 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 from .models import Employee
 
 class EmployeeCreate(CreateView):
     model = Employee
     fields = ['first_name', 'last_name', 'badges']
     success_url = '/admin'
+
+class EmployeeUpdate(UpdateView):
+    model = Employee
+    fields = ['first_name', 'last_name', 'badges']
+    success_url = '/admin'
+
+class EmployeeList(ListView):
+    model = Employee
+    paginate_by = 100
