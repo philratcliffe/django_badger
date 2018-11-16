e5ff6c852d3b2ff38fcef1d975a67017c853aa8d
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-16 16:50:47 +0000

    Made URL naming more consistent.

--- badger/urls.py
+++ badger/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='badger/index.html'), name='index'),
     path('employee_create/', views.EmployeeCreate.as_view(), name='employee_create'),
     path('employee_update/<int:pk>/', views.EmployeeUpdate.as_view(), name='employee_update'),
-    path('employees_list/', views.EmployeeList.as_view(), name='employees_list'),
+    path('employee_list/', views.EmployeeList.as_view(), name='employee_list'),
     path('badge_create/', views.BadgeCreate.as_view(), name='badge_create'),
     path('badge_update/<int:pk>/', views.BadgeUpdate.as_view(), name='badge_update'),
     path('badge_list/', views.BadgeList.as_view(), name='badge_list'),
