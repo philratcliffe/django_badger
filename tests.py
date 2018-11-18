f2a3bb0f789b0c0879d7f28015c9cfccd4beec49
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-18 11:40:54 +0000

    Add an initial test.

---
+++ tests.py
@@ -0,0 +1,9 @@
+from django.urls import resolve
+from django.test import TestCase
+from django.views.generic import TemplateView
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_template_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func.__name__, "TemplateView")
