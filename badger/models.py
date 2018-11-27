57579de1a16720517d02fd53d58313cf385e8613
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-27 10:57:50 +0000

    Add a slug field for Employees

--- badger/models.py
+++ badger/models.py
@@ -1,4 +1,5 @@
 from model_utils.models import TimeStampedModel
+from django.template.defaultfilters import slugify
 from django.db import models
 
 class Badge(TimeStampedModel):
@@ -14,6 +15,11 @@ class Employee(TimeStampedModel):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     badges = models.ManyToManyField(Badge, blank=True)
+    slug = models.SlugField(unique=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify("{} {} {}".format(self.first_name, self.last_name, self.pk))
+        super(Employee, self).save(*args, **kwargs)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
