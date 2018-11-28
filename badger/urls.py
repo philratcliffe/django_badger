1374262702f6687f542820bf62bf82fc6fca0edd
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-28 08:54:35 +0000

    Add slugs to employee URLs

--- badger/urls.py
+++ badger/urls.py
@@ -8,7 +8,7 @@ app_name = 'badger'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='badger/index.html'), name='index'),
     path('employee_create/', views.EmployeeCreate.as_view(), name='employee_create'),
-    path('employee_update/<int:pk>/', views.EmployeeUpdate.as_view(), name='employee_update'),
+    path('employee_update/<slug:slug>/', views.EmployeeUpdate.as_view(), name='employee_update'),
     path('employee_list/', views.EmployeeList.as_view(), name='employee_list'),
     path('badge_create/', views.BadgeCreate.as_view(), name='badge_create'),
     path('badge_update/<int:pk>/', views.BadgeUpdate.as_view(), name='badge_update'),
