02b513281bc608069de8e824a0290938e9a80370
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-01 13:50:42 +0000

    PEP8 format fixes for views

--- badger/views.py
+++ badger/views.py
@@ -4,29 +4,35 @@ from django.views.generic.list import ListView
 from .models import Employee
 from .models import Badge
 
+
 class EmployeeCreate(CreateView):
     model = Employee
     fields = ['first_name', 'last_name', 'badges']
     success_url = '/admin'
+
 
 class EmployeeUpdate(UpdateView):
     model = Employee
     fields = ['first_name', 'last_name', 'badges']
     success_url = reverse_lazy('badger:employee_list')
 
+
 class EmployeeList(ListView):
     model = Employee
     paginate_by = 100
+
 
 class BadgeCreate(CreateView):
     model = Badge
     fields = ['name']
     success_url = '/admin'
 
+
 class BadgeUpdate(UpdateView):
     model = Badge
     fields = ['name']
     success_url = reverse_lazy('badger:badge_list')
+
 
 class BadgeList(ListView):
     model = Badge
