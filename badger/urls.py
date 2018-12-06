265c1ef323a23e580b8de08c2e38f37e85677ad6
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-06 09:59:19 +0000

    Add Detail view

--- badger/urls.py
+++ badger/urls.py
@@ -8,6 +8,7 @@ app_name = 'badger'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='badger/index.html'), name='index'),
     path('employee_create/', views.EmployeeCreate.as_view(), name='employee_create'),
+    path('employee_detail/<slug:slug>/', views.EmployeeDetail.as_view(), name='employee_detail'),
     path('employee_update/<slug:slug>/', views.EmployeeUpdate.as_view(), name='employee_update'),
     path('employee_list/', views.EmployeeList.as_view(), name='employee_list'),
     path('badge_create/', views.BadgeCreate.as_view(), name='badge_create'),
--- badger/views.py
+++ badger/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Employee
 from .models import Badge
 
@@ -9,6 +10,11 @@ class EmployeeCreate(CreateView):
     model = Employee
     fields = ['first_name', 'last_name', 'badges']
     success_url = reverse_lazy('badger:employee_list')
+
+
+class EmployeeDetail(DetailView):
+    model = Employee
+    fields = ['first_name', 'last_name', 'badges']
 
 
 class EmployeeUpdate(UpdateView):
