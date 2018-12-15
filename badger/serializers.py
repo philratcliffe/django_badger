7e1df3d12dc5ae8b61763527f7c54acfc9f6e11a
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-15 13:30:05 +0000

    Add Badge Serializer

--- badger/serializers.py
+++ badger/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from .models import Badge
 from .models import Employee
 
+class BadgeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Badge
+        fields = ("name", )
+
+
 class EmployeeSerializer(serializers.ModelSerializer):
+    badges = BadgeSerializer(read_only=True, many=True)
+
+
     class Meta:
         model = Employee
-        fields = ('first_name', 'last_name')
+        fields = ("first_name", "last_name", "badges", "slug" )
