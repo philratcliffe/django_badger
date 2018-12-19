3556820152603dfb5b093098433d85b425372c64
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-19 11:51:16 +0000

    Redirect no url to badger app

--- django_badger/urls.py
+++ django_badger/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from django.conf import settings
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 import badger
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', RedirectView.as_view(pattern_name='badger:employee_list', permanent=True)),
     path('admin/', admin.site.urls),
     path('badger/', include('badger.urls', namespace='badger')),
 ]
