a41fb8883f1b65c5644f7662f4c5197b2925e3ab
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-23 09:10:53 +0000

    Return id via api

--- badger/api/serializers.py
+++ badger/api/serializers.py
@@ -7,14 +7,13 @@ class BadgeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Badge
-        fields = ("name", )
+        fields = ("id", "name", )
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     badges = BadgeSerializer(read_only=True, many=True)
 
-
     class Meta:
         model = Employee
-        fields = ("first_name", "last_name", "badges", "slug" )
+        fields = ("id", "first_name", "last_name", "badges", "slug" )
         read_only_fields = ('slug',)
