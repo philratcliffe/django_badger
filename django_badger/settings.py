92bdcace2d4502bb149d7879ee2f7c206354e1d1
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-28 00:40:22 +0000

    Set default auth and permissions policy for api

--- django_badger/settings.py
+++ django_badger/settings.py
@@ -128,3 +128,12 @@ STATIC_URL = '/static/'
 INTERNAL_IPS = ['127.0.0.1', ]
 LOGIN_REDIRECT_URL = 'users:user_detail'
 AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+	'rest_framework.authentication.BasicAuthentication',
+    ),
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
