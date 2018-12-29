2bb408b6f12e489f8259d1c9b01e2d4ff8283acf
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-29 09:39:17 +0000

    Restrict adding users via api to admins only.

--- users/api/views.py
+++ users/api/views.py
@@ -1,11 +1,16 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
 from users.models import CustomUser
 from .serializers import CustomUserSerializer
 
+
+class ReadOnlyAndAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS and request.user.is_authenticated
+
+
 class CustomUserList(generics.ListCreateAPIView):
+    permission_classes = (IsAdminUser| ReadOnlyAndAuthenticated, )
+
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-
