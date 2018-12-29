d107e85bc06ab689a589de6ac06e20d4c3953255
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-29 09:53:30 +0000

    Add script to test listing users

--- api_test_scripts/add_user.sh
+++ api_test_scripts/add_user.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -u user:password -H "Content-Type: application/json" -X POST --data '{"email": "foo1@bar.com", "password": "foobar", "username": "foobar1"}' http://127.0.0.1:8000/users/api/
+# Need to be an admin to add a user
+curl -u admin2:somepasswd -H "Content-Type: application/json" -X POST --data '{"email": "test1@test1.com", "password": "Xcy2bfgdds", "username": "test1"}' http://127.0.0.1:8000/users/api/
 
---
+++ api_test_scripts/list_users.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -u user1:somepasswd http://127.0.0.1:8000/users/api/
+
