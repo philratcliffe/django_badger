b1d09587f1690211eee129a178b61e723c58153f
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-12-30 10:04:54 +0000

    Set password correctly via API

--- api_test_scripts/add_user.sh
+++ api_test_scripts/add_user.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-# Need to be an admin to add a user
-curl -u admin2:somepasswd -H "Content-Type: application/json" -X POST --data '{"email": "test1@test1.com", "password": "Xcy2bfgdds", "username": "test1"}' http://127.0.0.1:8000/users/api/
+# Development testing script
+# Note: admin privileges are required to add a user 
+USER=$1
+EMAIL="$USER@test.com"
+
+
+curl -i \
+-u admin2:somepasswd \
+-H "Content-Type: application/json" \
+-X POST --data '{"email": "'"$EMAIL"'", "password": "notsecret", "username": "'"$USER"'"}' http://127.0.0.1:8000/users/api/
 
--- users/api/serializers.py
+++ users/api/serializers.py
@@ -6,5 +6,15 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "email", "first_name", "last_name")
+        fields = ("username", "email", "password")
+        extra_kwargs = {'password': {'write_only': True}}
 
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
